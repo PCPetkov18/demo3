7d00075039ec45e49eece7348c5ad0cbd13a3982
PCPetkov18 <PCPetkov18@codingburgas.bg>
2022-11-02 15:39:38 +0200

    All the classes

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo3Application.class, args);
+        SpringApplication.run(RecommendationsController.class, args);
     }
 
 }
